746779e2ea4bea3776fe2715a568c6acf288de31
bejaranita <bejaranita@gmail.com>
2026-04-23 02:56:45 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,17 @@
-// Service Worker para Momentum
-const CACHE_NAME = 'momentum-v1';
-const urlsToCache = [
-  './',
-  './momentum.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'momentum-v5';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache).catch(() => {
-        console.log('Cache de algunos recursos falló, pero la app seguirá funcionando');
-      });
-    })
-  );
+self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(response => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      }).catch(() => {
-        return caches.match(event.request);
-      });
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
