97e2c4d58200ff3d88202dda5489c51aaa27f6a3
bejaranita <bejaranita@gmail.com>
2026-05-02 21:08:40 -0500

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,17 +1,28 @@
-const CACHE_NAME = 'momentum-v5';
+const CACHE_NAME = 'momentum-v6';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    ).then(() => self.clients.claim())
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
 });
